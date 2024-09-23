58b62237f2618fc27c27a049a7b7213dc137f866
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-09-23 15:08:48 +0530

    Time: 122 ms (56.68%), Space: 64.7 MB (70.67%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cpp
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -4,18 +4,18 @@ public:
         if(n==1){
             return 1;
         }
-        vector<pair<set<int>,set<int>>> adjl(n+1);
-        for(int i=0;i<trust.size();i++){
-            adjl[trust[i][1]].first.insert(trust[i][0]);
-            adjl[trust[i][0]].second.insert(trust[i][1]);
+        pair<int,int> p={0,0};
+      vector<pair<int,int>> t(n+1,{0,0});
+      for(int i=0;i<trust.size();i++){
+        t[trust[i][0]].first+=1;
+        t[trust[i][1]].second+=1;
+      }
+      for(int i=0;i<t.size();i++){
+        cout<<i<<"->"<<t[i].first<<" "<<t[i].second<<endl;
+        if(t[i].first==0 && t[i].second==n-1){
+            return i;
         }
-        int c=0;
-        for(auto i:adjl){
-            if(i.first.size()==n-1 && i.second.size()==0){
-               return c; 
-            }
-            c++;
-        }
+      }
         return -1;
     }
 };
