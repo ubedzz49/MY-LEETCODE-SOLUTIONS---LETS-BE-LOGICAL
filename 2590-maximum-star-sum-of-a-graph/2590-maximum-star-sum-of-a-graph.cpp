c833d3ecf7337e4220d299d6e5e0d3856a09528e
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-10-15 01:36:41 +0530

    Time: 614 ms (5.25%), Space: 149.3 MB (21.95%) - LeetHub

---
+++ 2590-maximum-star-sum-of-a-graph/2590-maximum-star-sum-of-a-graph.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxStarSum(vector<int>& vals, vector<vector<int>>& edges, int k) {
+        vector<vector<int>> adj(vals.size());
+        for(auto i:edges){
+            adj[i[0]].push_back(vals[i[1]]);
+            adj[i[1]].push_back(vals[i[0]]);
+        }
+        int kk=0;
+        for(auto i:adj){
+            vector<int> it=i;
+            sort(it.begin(),it.end());
+            reverse(it.begin(),it.end());
+            adj[kk]=it;
+            cout<<"for "<<kk<<"-> ";
+            for(auto ii:adj[kk]){
+                cout<<ii<<" ";
+            }
+            cout<<endl;
+            kk++;
+        }
+        int mx=INT_MIN;
+        for(int i=0;i<vals.size();i++){
+            int sum=vals[i];
+            mx=max(mx,sum);
+            for(int j=0;j<k && j<adj[i].size();j++){
+                sum+=adj[i][j];
+                mx=max(mx,sum);
+            }
+        }
+        return mx;
+    }
+};
