26945162fa67062dce8a2125885f2e692fcfcbb2
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-04 03:20:16 +0530

    Time: 0 ms (100%), Space: 10 MB (40.98%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
-void backtrack(unordered_set<int>& sett,vector<vector<int>>& ans,vector<int>& path){
-    if(sett.size()==0) {
+// void backtrack(unordered_set<int>& sett,vector<vector<int>>& ans,vector<int>& path){
+//     if(sett.size()==0) {
+//         ans.push_back(path);
+//         return;
+//     }
+//     unordered_set<int> settt=sett;
+//     for(auto i:settt){
+//         int it=i;
+//         path.push_back(it);
+//         sett.erase(it);
+//         backtrack(sett,ans,path);
+//         path.pop_back();
+//         sett.insert(it);
+//     }
+// }
+
+//OPTIMAL SOLUTION
+void backtrack(vector<vector<int>>& ans,vector<int>& path,vector<int>& nums,vector<bool>& visited){
+    if(path.size()==nums.size()){
         ans.push_back(path);
         return;
     }
-    unordered_set<int> settt=sett;
-    for(auto i:settt){
-        int it=i;
-        path.push_back(it);
-        sett.erase(it);
-        backtrack(sett,ans,path);
-        path.pop_back();
-        sett.insert(it);
+    for(int i=0;i<visited.size();i++){
+        if(!visited[i]){
+            visited[i]=1;
+            path.push_back(nums[i]);
+            backtrack(ans,path,nums,visited);
+            path.pop_back();
+            visited[i]=0;
+        }
     }
 }
     vector<vector<int>> permute(vector<int>& nums) {
-        unordered_set<int> sett;
-        for(auto it:nums) sett.insert(it);
+        // unordered_set<int> sett;
+        // for(auto it:nums) sett.insert(it);
+        // vector<vector<int>> ans;
+        // vector<int> path;
+        // backtrack(sett,ans,path);
+        // return ans;
+
+        //OPTIMAL SOLUTION
         vector<vector<int>> ans;
         vector<int> path;
-        backtrack(sett,ans,path);
+        vector<bool> visited(nums.size(),0);
+        backtrack(ans,path,nums,visited);
         return ans;
     }
 };
