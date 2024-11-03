4a571bfeac22027bd6da51abd5a19e4f560b9361
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-04 04:48:28 +0530

    Time: 14 ms (13.25%), Space: 14.8 MB (10.28%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,48 @@
+
+
+class Solution {
+public:
+bool ispos(int n,int i,int j){
+    if(i>=0 && j>=0 && i<n && j<n) return true;
+    return false;
+}
+void place(vector<string>& curr,int& i,int& j,int& n){
+    curr[i][j]='Q';
+    for(int it=0;it<n;it++){
+        if(ispos(n,it,j)) curr[it][j]='.';
+        if(ispos(n,i,it)) curr[i][it]='.';
+    }
+    for(int it=1;i-it>=0;it++){
+        if(ispos(n,i-it,j-it)) curr[i-it][j-it]='.';
+        if(ispos(n,i-it,j+it)) curr[i-it][j+it]='.';
+    }
+    for(int it=1;i+it<n;it++){
+        if(ispos(n,i+it,j-it)) curr[i+it][j-it]='.';
+        if(ispos(n,i+it,j+it)) curr[i+it][j+it]='.';
+    }
+    curr[i][j]='Q';
+}
+void back(vector<vector<string>>& ans,vector<string>& curr,int& i,int& n){
+    if(i==n){
+        ans.push_back(curr);
+        return;
+    }
+    for(int j=0;j<n;j++){
+        if(curr[i][j]==' '){
+            vector<string> v=curr;
+            place(curr,i,j,n);
+            i++;
+            back(ans,curr,i,n);
+            i--;
+            curr=v;
+        }
+    }
+}
+    int totalNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> curr(n,string(n,' '));
+        int i=0;
+        back(ans,curr,i,n);
+        return ans.size();
+    }
+};
