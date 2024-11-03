0ec2de4581f9eb35bb2e8d64c0df2fa532086571
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-03 21:49:56 +0530

    Time: 2 ms (15.25%), Space: 7.9 MB (33.55%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size()) return false;
        s=s+s;
-       if(s.size()!=goal.size()*2){
-        return false;
-       }
        for(int i=0;i<s.size()-goal.size();i++){
         if(s.substr(i,goal.size())==goal){
             return true;
