5f610d7b00497b694721d429efc20f99f90c6093
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-04 16:27:10 +0530

    Time: 2 ms (20.84%), Space: 16.6 MB (33.73%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        for(int k=0;k<graph.size();k++){
         vector<int> color(graph.size(),-1);
-        queue<int> q;
-        q.push(k);
-        color[k]=0;
+               
+        for(int i=0;i<graph.size();i++){
+            if(color[i]==-1){
+                queue<int> q;
+        q.push(i);
+        color[i]=0;
         while(!q.empty()){
-            int c;
             int n=q.front();
-            if(color[n]==0){
-            c=1;
-        }else{
-            c=0;
-        }
-            
             q.pop();
-            for(int i=0;i<graph[n].size();i++){
-                if(color[graph[n][i]]==-1){
-                    q.push(graph[n][i]);
-                    color[graph[n][i]]=c;
-                    continue;
-                }
-                else if(c!=color[graph[n][i]]){
-                    return false;
+            int c=color[n];
+            if(c==0) {c=1;}
+            else{c=0;}
+            for(auto it:graph[n]){
+                if(color[it]==-1){
+                    color[it]=c;
+                    q.push(it);
+                }else{
+                    if(color[it]!=c) return false;
                 }
             }
         }
+            }
         }
+        for(auto it:color) cout<<it<<" ";
         return true;
     }
 };
