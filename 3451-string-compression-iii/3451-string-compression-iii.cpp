fed1456de220939d2c469fa79e3e849ef54e3092
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-04 12:28:06 +0530

    Time: 17 ms (81.2%), Space: 31.2 MB (5.39%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string compressedString(string s) {
+        s.push_back('1');
+        string ans="";
+        int c=0;
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i]==s[i+1]){
+                c++;
+            }else{
+                c++;
+                ans.push_back((c+'0'));
+                ans.push_back(s[i]);
+                c=0;
+            }
+            if(c==9){
+                ans.push_back((c+'0'));
+                ans.push_back(s[i]);
+                c=0;
+            }
+        }
+        return ans;
+    }
+};
