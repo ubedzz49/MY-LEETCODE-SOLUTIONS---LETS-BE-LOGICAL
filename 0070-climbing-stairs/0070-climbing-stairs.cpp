7ac381e56a72cc060de42bc84fe4e486859d1b83
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-05 12:38:55 +0530

    Time: 0 ms (100%), Space: 7.4 MB (88.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,40 @@
+//MEMOIZATION
+// class Solution {
+// public:
+// int dy(vector<int>& dp,int n){
+//     if(n<=1) return 1;
+//     if(dp[n]==-1){
+//         dp[n]=dy(dp,n-1)+dy(dp,n-2);
+//     }
+//     return dp[n];
+// }
+//     int climbStairs(int n) {
+//         if(n<=1) return 1;
+//         vector<int> dp(n+1,-1);
+//         dp[0]=1;
+//         dp[1]=1;
+//         return dy(dp,n);
+//     }
+// };
+
+
+
+//----------------------------------
+
+
+//TABULATION 
 class Solution {
 public:
-int dy(vector<int>& dp,int n){
-    if(n<=1) return 1;
-    if(dp[n]==-1){
-        dp[n]=dy(dp,n-1)+dy(dp,n-2);
-    }
-    return dp[n];
-}
     int climbStairs(int n) {
         if(n<=1) return 1;
-        vector<int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
-        return dy(dp,n);
+        int a=1;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+            if(i==n) return c;
+        }
+        return 0;
     }
 };
