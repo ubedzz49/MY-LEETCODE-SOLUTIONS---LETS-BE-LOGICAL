ad389b398267642a0c5f075ca29de1f7622e1cf1
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-07 19:01:52 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.5%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
 
-//MEMOIZATION
-int memo(vector<vector<long long>>& dp,vector<vector<int>>& nums,int i,int j){
-   // if(nums[i][j]==1) return 0;
-    if(i==nums.size()-1 && j==nums[0].size()-1){return dp[i][j]=1;}
-    long long ans=0;
-    if(i+1<nums.size() && nums[i+1][j]!=1){
-        if(dp[i+1][j]==-1) ans+=memo(dp,nums,i+1,j);
-        else{ans+=dp[i+1][j];}
-    }
-    if(j+1<nums[0].size() && nums[i][j+1]!=1){
-        if(dp[i][j+1]==-1) ans+=memo(dp,nums,i,j+1);
-        else{ans+=dp[i+1][j];}
-    }
-    return dp[i][j]=ans;
-}
+// //MEMOIZATION
+// int memo(vector<vector<long long>>& dp,vector<vector<int>>& nums,int i,int j){
+//    // if(nums[i][j]==1) return 0;
+//     if(i==nums.size()-1 && j==nums[0].size()-1){return dp[i][j]=1;}
+//     long long ans=0;
+//     if(i+1<nums.size() && nums[i+1][j]!=1){
+//         if(dp[i+1][j]==-1) ans+=memo(dp,nums,i+1,j);
+//         else{ans+=dp[i+1][j];}
+//     }
+//     if(j+1<nums[0].size() && nums[i][j+1]!=1){
+//         if(dp[i][j+1]==-1) ans+=memo(dp,nums,i,j+1);
+//         else{ans+=dp[i+1][j];}
+//     }
+//     return dp[i][j]=ans;
+// }
 
 
     int uniquePathsWithObstacles(vector<vector<int>>& nums) {
 
-        vector<vector<long long>> dp(nums.size(),vector<long long>(nums[0].size(),-1));
-        if(nums[nums.size()-1][nums[0].size()-1]==0 && nums[0][0]==0){
-            dp[nums.size()-1][nums[0].size()-1]==1;
-            long long ans=memo(dp,nums,0,0);
-            return ans;
-        }
-        else return 0;
+        // //MEMOIZATION
+        // vector<vector<long long>> dp(nums.size(),vector<long long>(nums[0].size(),-1));
+        // if(nums[nums.size()-1][nums[0].size()-1]==0 && nums[0][0]==0){
+        //     dp[nums.size()-1][nums[0].size()-1]==1;
+        //     long long ans=memo(dp,nums,0,0);
+        //     return ans;
+        // }
+        // else return 0;
 
 
         // //TABULATION
@@ -42,5 +43,26 @@ int memo(vector<vector<long long>>& dp,vector<vector<int>>& nums,int i,int j){
         //     }
         // }
         // return dp[0][0];
+
+
+
+
+
+        //SPACE OPTIMIZATION WITH TABULATION
+        if(nums[nums.size()-1][nums[0].size()-1]==1 || nums[0][0]==1) return 0;
+        int r=nums.size();
+        int c=nums[0].size();
+        vector<long long> curr(r,0);
+        curr[r-1]=1;
+        for(int j=c-1;j>=0;j--){
+            vector<long long> next(r,0);
+            for(int i=r-1;i>=0;i--){
+                if(i>0 && nums[i-1][j]==0) curr[i-1]+=curr[i];
+                if(j>0 && nums[i][j-1]==0) next[i]+=curr[i];
+            }
+            if(j==0) return curr[0];
+            curr=next;
+        }
+        return 0;
     }
 };
