164e9c0b381cb9f28aa0f030c67f272bd6bf3612
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-08 03:07:48 +0530

    Time: 3 ms (31.26%), Space: 14.8 MB (68.81%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-int memo(vector<vector<int>>& dp,vector<vector<int>>& nums,int i,int j){
-    if(i==nums.size()-1 && j==nums[0].size()-1) return nums[i][j];
-    int ans=INT_MAX;
-    if(i+1<nums.size()){
-        if(dp[i+1][j]==-1) memo(dp,nums,i+1,j);
-        ans=dp[i+1][j];
-    }
-    if(j+1<nums[0].size()){
-        if(dp[i][j+1]==-1) memo(dp,nums,i,j+1);
-        ans=min(ans,dp[i][j+1]);
-    }
-    return dp[i][j]=ans+nums[i][j];
-}
+// //MEMOIZATION
+// int memo(vector<vector<int>>& dp,vector<vector<int>>& nums,int i,int j){
+//     if(i==nums.size()-1 && j==nums[0].size()-1) return nums[i][j];
+//     int ans=INT_MAX;
+//     if(i+1<nums.size()){
+//         if(dp[i+1][j]==-1) memo(dp,nums,i+1,j);
+//         ans=dp[i+1][j];
+//     }
+//     if(j+1<nums[0].size()){
+//         if(dp[i][j+1]==-1) memo(dp,nums,i,j+1);
+//         ans=min(ans,dp[i][j+1]);
+//     }
+//     return dp[i][j]=ans+nums[i][j];
+// }
+
     int minPathSum(vector<vector<int>>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums[0].size(),-1));
+        // //MEMOIZATION
+        // vector<vector<int>> dp(nums.size(),vector<int>(nums[0].size(),-1));
+        // dp[nums.size()-1][nums[0].size()-1]=nums[nums.size()-1][nums[0].size()-1];
+        // return memo(dp,nums,0,0);
+
+        //TABULATION
+        vector<vector<int>> dp(nums.size(),vector<int>(nums[0].size(),INT_MAX));
         dp[nums.size()-1][nums[0].size()-1]=nums[nums.size()-1][nums[0].size()-1];
-        return memo(dp,nums,0,0);
+        //cout<<dp[nums.size()-1][nums[0].size()-1];
+        for(int j=nums[0].size()-1;j>=0;j--){
+            for(int i=nums.size()-1;i>=0;i--){
+                 if(i>0) dp[i-1][j]=min(dp[i-1][j],nums[i-1][j]+dp[i][j]);
+                 if(j>0) dp[i][j-1]=min(dp[i][j-1],nums[i][j-1]+dp[i][j]);
+            }
+        }
+        return dp[0][0];
     }
 };
