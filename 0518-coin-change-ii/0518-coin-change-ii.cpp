2f851672c137de6bec7cd3939578020d8b98a098
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-12 18:17:34 +0530

    Time: 7 ms (85.6%), Space: 9.2 MB (84.75%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int change(int sum, vector<int>& coins) {
         //SPACE OPTIMISED APPROACT USING ONLY ONE VECTOR
-        if(sum==4681) return 0;
-        vector<long long> dp(sum+1,0);
+        vector< unsigned long long> dp(sum+1,0);
         dp[0]=1;
         for(auto c:coins){
             for(int i=0;i<=sum;i++){
