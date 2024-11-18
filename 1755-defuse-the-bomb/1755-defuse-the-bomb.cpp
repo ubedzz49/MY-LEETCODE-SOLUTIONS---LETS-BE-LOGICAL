46cf97dbdb2598ce9472930313e7fcc35d43cf2c
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-18 08:50:58 +0530

    Time: 0 ms (100%), Space: 10.4 MB (17.93%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -8,11 +8,9 @@ public:
         if(k<0) k=-k2;
         for(int i=0;i<k;i++)sum+=code[i];
         for(int i=k;i<k+code.size();i++){
-            cout<<i<<" ";
             if(k2<0) {code[(i)%code.size()]=sum;}
             int temp1=code1[(i-k)%code.size()]; 
             int temp2=code1[i%code.size()];
-            
             sum+=temp2;
             sum-=temp1;
             if(k2>0) {code[(i-k)%code.size()]=sum;}
