8ac45cf5a679254db55138a118af708b2c5bfe90
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-21 13:25:01 +0530

    Time: 4 ms (76.26%), Space: 12.3 MB (82.64%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<int> dp(word2.size()+1,0);
+        for(int i=0;i<dp.size();i++) dp[i]=i;
+        for(int i=1;i<word1.size()+1;i++){
+            vector<int> dp2=dp;
+            dp2[0]++;
+            for(int j=1;j<word2.size()+1;j++){
+                if(word1[i-1]==word2[j-1]) dp2[j]=dp[j-1];
+                else dp2[j]=min(dp[j-1],min(dp2[j-1],dp[j]))+1;
+            }
+            dp=dp2;
+        }
+        return dp[word2.size()];
+    }
+};
