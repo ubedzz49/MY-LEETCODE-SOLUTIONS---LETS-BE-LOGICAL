9cc4ab16c1f1afc7c68468a3395ac8fec06534a2
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-11-26 01:04:16 +0530

    Time: 71 ms (21.84%), Space: 16.5 MB (72.69%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    bool isMatch(string text, string pattern) {
-        //
-        // vector<vector<bool>> dp(s.size()+1,vector<bool>(p.size()+1,0));
-        // dp[0][0]=1;
-        // for(int i=0;i<p.size();i++) {
-        //     if(p[i]=='*') dp[0][i+1]=1;
-        //     else break;
-        // }
-        // for(int i=1;i<=s.size();i++){
-        //     for(int j=1;j<=p.size();j++){
-        //         if(p[j-1]=='?' || p[j-1]==s[i-1]) dp[i][j]=dp[i-1][j-1];
-        //         else if(p[j-1]=='*') dp[i][j]=(dp[i-1][j-1] || dp[i][j-1] || dp[i-1][j]);
-        //     }
-        // }
-        // return dp[s.size()][p.size()];
-        vector<bool> dp(pattern.size()+1,0);
-   for (int i = 0; i < pattern.size(); i++) {
-     if (pattern[i] == '*') dp[i + 1] = 1;
-     else break;
-   }
-   dp[0]=1;
-      for(int i=0;i<text.size();i++){
-         vector<bool> dp2=dp;
-         dp[0]=0;
-         for(int j=1;j<dp.size();j++){
-            if(pattern[j-1]=='?' || pattern[j-1]==text[i]) dp[j]=dp2[j-1];
-            else if(pattern[j-1]=='*') dp[j]=dp[j-1]||dp2[j-1]||dp2[j];
-            else dp[j]=false;
-         }
-      }
-   return dp[dp.size()-1];
+    bool isMatch(string s, string p) {
+        //TABULATION USING A DP MATRIX (WITHOUT SPACE OPTIMIZATION)
+        //TABULATION WITH SPACE OPTIMIZATION AT CODE360 USING ONLY 2 VECTORS
+        vector<vector<bool>> dp(s.size()+1,vector<bool>(p.size()+1,0));
+        dp[0][0]=1;
+        for(int i=0;i<p.size();i++) {
+            if(p[i]=='*') dp[0][i+1]=1;
+            else break;
+        }
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=p.size();j++){
+                if(p[j-1]=='?' || p[j-1]==s[i-1]) dp[i][j]=dp[i-1][j-1];
+                else if(p[j-1]=='*') dp[i][j]=(dp[i-1][j-1] || dp[i][j-1] || dp[i-1][j]);
+            }
+        }
+        return dp[s.size()][p.size()];
+      
     }
 };
