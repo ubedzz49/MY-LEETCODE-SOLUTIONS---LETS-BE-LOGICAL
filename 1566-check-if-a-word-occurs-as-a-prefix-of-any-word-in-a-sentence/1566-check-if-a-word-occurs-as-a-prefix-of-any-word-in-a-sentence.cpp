914cdbe278249dd65288e98434c9cace78047fa8
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-12-02 14:35:42 +0530

    Time: 0 ms (100%), Space: 8.2 MB (7.8%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
-        unordered_map<string,int> mpp;
         string word="";
         int c=1;
         for(auto it:sentence){
