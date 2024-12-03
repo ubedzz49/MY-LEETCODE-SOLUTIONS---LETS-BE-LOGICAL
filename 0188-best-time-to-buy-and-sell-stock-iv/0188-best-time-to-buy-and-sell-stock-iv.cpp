823cf28019101fe6c9832b53dcf40a7a18318125
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-12-04 01:00:55 +0530

    Time: 0 ms (100%), Space: 14.6 MB (70.4%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -7,17 +7,13 @@ public:
             dp[0][0]=max(dp[0][0],-it);
             dp[0][1]=max(0,dp[0][1]);
             dp[0][1]=max(dp[0][1],dp[0][0]+it);
-            cout<<dp[0][0]<<" "<<dp[0][1]<<endl;
             for(int i=1;i<k;i++){
                 dp[i][0]=max(dp[i][0],dp[i-1][1]-it);
                 dp[i][0]=max(dp[i][0],dp[i-1][0]);
                 dp[i][1]=max(dp[i][1],dp[i-1][1]);
                 dp[i][1]=max(dp[i][1],dp[i][0]+it);
-                cout<<dp[i][0]<<" "<<dp[i][1]<<endl;
             }
-            cout<<"----"<<endl;
         }
-        for(auto it:dp) cout<<it[0]<<" "<<it[1]<<"  ";
         return dp[k-1][1];
     }
 };
