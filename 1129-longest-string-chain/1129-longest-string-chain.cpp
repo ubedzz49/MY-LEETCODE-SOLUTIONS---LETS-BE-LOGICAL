2b482f43244d8c258ecec038bd8b47ea24ba3c37
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2024-12-10 16:35:53 +0530

    Time: 1891 ms (14.39%), Space: 651 MB (7.22%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+static bool comparator(const string w1,const string w2){
+    return w1.size()<w2.size();
+}
+bool isfine(string w1,string w2){
+    if(w1.size()!=w2.size()-1) return 0;
+    int un=0;
+    int c=0;
+    for(auto it:w2){
+        if(it==w1[c]) c++;
+        else un++;
+    }
+    return un==1;
+} 
+    int longestStrChain(vector<string>& words) {
+        vector<int> dp(words.size(),1);
+        sort(words.begin(),words.end(),comparator);
+        for(int i=1;i<words.size();i++){
+            for(int j=0;j<i;j++){
+                if(isfine(words[j],words[i]) && dp[i]<dp[j]+1)dp[i]=dp[j]+1;
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
+
+
+
+
+// //BRUTE FORCE SOLUTION BELOW 
+// //MEMORY LIMIT EXCEEDS AT 76/86
+// class Solution {
+// public:
+// static bool comparator(const string w1,const string w2){
+//     return w1.size()<w2.size();
+// }
+// bool isfine(string w1,string w2){
+//     if(w1.size()!=w2.size()-1) return 0;
+//     vector<vector<int>> dp(w1.size()+1,vector<int> (w2.size()+1,0));
+//     for(int i=1;i<dp.size();i++){
+//         for(int j=1;j<dp[0].size();j++){
+//             if(w1[i-1]==w2[j-1]) dp[i][j]=max(dp[i][j],dp[i-1][j-1]+1);
+//             else dp[i][j]=max(dp[i][j],max(dp[i-1][j-1],max(dp[i-1][j],dp[i][j-1])));
+//         }
+//     }
+//     if(dp[dp.size()-1][dp[0].size()-1]==w1.size()) return 1;
+//     return 0;
+// } 
+//     int longestStrChain(vector<string>& words) {
+//         vector<int> dp(words.size(),1);
+//         sort(words.begin(),words.end(),comparator);
+//         for(int i=1;i<words.size();i++){
+//             for(int j=0;j<i;j++){
+//                 if(isfine(words[j],words[i]) && dp[i]<dp[j]+1)dp[i]=dp[j]+1;
+//             }
+//         }
+//         return *max_element(dp.begin(),dp.end());
+//     }
+// };
