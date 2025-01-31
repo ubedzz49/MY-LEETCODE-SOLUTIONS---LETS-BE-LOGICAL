bb8bbd15bd0a7ba687c0853daaf9074ed38f2ffd
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-01-31 14:20:07 +0530

    Time: 0 ms (100%), Space: 28.1 MB (60.53%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -3,11 +3,9 @@ public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int ans=0;
         queue<pair<int,int>> q;
-        //vector<vector<bool>> vis(grid.size(),vector<bool> (grid[0].size(),0));
         vector<int> x={-1,+1,0,0};
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                //if(grid[i][j]==0) vis[i][j]=1;
                 if( grid[i][j]!=0){
                     int curr=0;
                    q.push(make_pair(i,j));
@@ -16,10 +14,6 @@ public:
                     int r=q.front().first;
                     int c=q.front().second;
                     q.pop();
-                    // if(grid[r][c]==0){
-                    //     grid[r][c]=0;
-                    // }
-                    // else{
                         curr+=1;
                         for(int ii=0;ii<4;ii++){
                             int xc=r+x[ii];
@@ -29,7 +23,6 @@ public:
                                 q.push(make_pair(xc,yc));
                             }
                         }
-                    //}
                    } 
                    ans=max(ans,curr);
                 }
