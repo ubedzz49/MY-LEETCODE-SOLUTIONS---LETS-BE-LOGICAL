d92fb8652308e7b6a3508f59085594c161d7c9ab
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-02-03 14:06:30 +0530

    Time: 11 ms (6.36%), Space: 25.2 MB (7.34%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool ispre(TreeNode*& root,int val){
+    if(root==nullptr) return 0;
+    if(root->val==val) return 1;
+    return ispre(root->left,val) || ispre(root->right,val);
+}
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        unordered_map<TreeNode*,TreeNode*> parent;
+        queue<pair<TreeNode*,TreeNode*>> q;
+        q.push(make_pair(root,root));
+        unordered_map<TreeNode*,bool> last;
+        while(!q.empty()){
+            unordered_map<TreeNode*,bool> last2;
+            last=last2;
+            int s=q.size();
+            while(s>0){
+                s--;
+                last[q.front().first]=1;
+                parent[q.front().first]=q.front().second;
+                if(q.front().first->left) q.push(make_pair(q.front().first->left,q.front().first));
+                if(q.front().first->right) q.push(make_pair(q.front().first->right,q.front().first));
+                q.pop();
+            }
+        }
+        TreeNode* ans=nullptr;
+        bool b=false;
+        if(last.size()==1){
+            for(auto it:last){
+                return it.first;
+            }
+        }
+        while(b==false){
+            unordered_map<TreeNode*,bool> last2;
+            for(auto it:last){
+                last2[parent[it.first]]=1;
+                ans=parent[it.first];
+            }
+            last=last2;
+            if(last.size()==1) b=true;
+        }
+        return ans;
+    }
+};
