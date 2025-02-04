7f431ed7b5eaeb8aee7edeb1532396353955bda2
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-02-04 13:41:50 +0530

    Time: 0 ms (100%), Space: 52.3 MB (60.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<bool> isvis(nums.size(),0);
-        isvis[0]=1;
+        // vector<bool> isvis(nums.size(),0);
+        // isvis[0]=1;
+        // for(int i=0;i<nums.size();i++){
+        //     if(isvis[i]==0) continue;
+        //     for(int j=1;j<=nums[i];j++){
+        //         if(i+j<isvis.size()) isvis[i+j]=1;
+        //         else break;
+        //     }
+        // }
+        // return isvis[isvis.size()-1];
+        int last=0;
         for(int i=0;i<nums.size();i++){
-            if(isvis[i]==0) continue;
-            for(int j=1;j<=nums[i];j++){
-                if(i+j<isvis.size()) isvis[i+j]=1;
-                else break;
-            }
+            if(i>last) return false;
+            last=max(last,i+nums[i]);
         }
-        return isvis[isvis.size()-1];
+        return true;
     }
 };
