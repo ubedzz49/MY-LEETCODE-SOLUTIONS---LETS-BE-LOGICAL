ec411473f52426a0369b4013949c852dbfa0b116
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-02-07 01:27:48 +0530

    Time: 4 ms (19.77%), Space: 21.8 MB (23.71%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& ints, vector<int>& newint) {
         ints.push_back(newint);
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        for(auto it:ints) pq.push(it);
-        int st=pq.top()[0];
-        int end=pq.top()[1];
-        pq.pop();
+        sort(ints.begin(),ints.end());
+        int st=ints[0][0];
+        int end=ints[0][1];
         vector<vector<int>> ans;
-        while(!pq.empty()){
-            int s=pq.top()[0];
-            int e=pq.top()[1];
-            pq.pop();
+        for(int i=1;i<ints.size();i++){
+            int s=ints[i][0];
+            int e=ints[i][1];
             if(s<=end) end=max(e,end);
             else{
                 ans.push_back({st,end});
@@ -19,6 +16,25 @@ public:
                 end=e;
             }
         }
+
+
+        // priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        // for(auto it:ints) pq.push(it);
+        // int st=pq.top()[0];
+        // int end=pq.top()[1];
+        // pq.pop();
+        // vector<vector<int>> ans;
+        // while(!pq.empty()){
+        //     int s=pq.top()[0];
+        //     int e=pq.top()[1];
+        //     pq.pop();
+        //     if(s<=end) end=max(e,end);
+        //     else{
+        //         ans.push_back({st,end});
+        //         st=s;
+        //         end=e;
+        //     }
+        // }
         ans.push_back({st,end});
         return ans;
     }
