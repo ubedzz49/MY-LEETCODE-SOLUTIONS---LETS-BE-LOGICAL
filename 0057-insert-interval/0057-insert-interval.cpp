257d9bc537bcffd168db38415fade909c796a2d5
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-02-07 01:32:54 +0530

    Time: 0 ms (100%), Space: 21.8 MB (47.91%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& ints, vector<int>& newint) {
-        ints.push_back(newint);
-        sort(ints.begin(),ints.end());
+           auto it = lower_bound(ints.begin(), ints.end(), newint, 
+           [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];});
+            ints.insert(it, newint);
         int st=ints[0][0];
         int end=ints[0][1];
         vector<vector<int>> ans;
@@ -16,25 +18,6 @@ public:
                 end=e;
             }
         }
-
-
-        // priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        // for(auto it:ints) pq.push(it);
-        // int st=pq.top()[0];
-        // int end=pq.top()[1];
-        // pq.pop();
-        // vector<vector<int>> ans;
-        // while(!pq.empty()){
-        //     int s=pq.top()[0];
-        //     int e=pq.top()[1];
-        //     pq.pop();
-        //     if(s<=end) end=max(e,end);
-        //     else{
-        //         ans.push_back({st,end});
-        //         st=s;
-        //         end=e;
-        //     }
-        // }
         ans.push_back({st,end});
         return ans;
     }
