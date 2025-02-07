987e1df7d12bd0142acaed5dee659d46bf9e1a1b
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-02-07 17:19:02 +0530

    Time: 39 ms (17.74%), Space: 24.1 MB (24.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
-
-bool ispos(int i,int j, vector<vector<char>>& grid){
-    if(i>=0 && i<grid.size() && j>=0 && j<grid[i].size() && grid[i][j]=='1'){
-        return true;
-    }
-    return false;
-}
     int numIslands(vector<vector<char>>& grid) {
-
-    int ans=0;
-    for(int i=0;i<grid.size();i++){
-        for(int j=0;j<grid[i].size();j++){
-            if(grid[i][j]=='0'){
-                continue;
-            }
-            ans++;
-            queue<pair<int,int>> q;
-            q.push({i,j});
-            grid[i][j]='0';
-            
-            while(!q.empty()){
-            int t=q.front().first;
-            int d=q.front().second;
-                if(ispos(t+1,d,grid)){
-                    q.push({t+1,d});
-                    grid[t+1][d]='0';
+        vector<vector<bool>> vis(grid.size(),vector<bool>(grid[0].size(),0));
+        int ans=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    ans++;
+                    vis[i][j]=1;
+                    vector<int> dir={-1,+1,0,0};
+                    queue<pair<int,int>> q;
+                    q.push(make_pair(i,j));
+                    while(!q.empty()){
+                        int ii=q.front().first;
+                        int jj=q.front().second;
+                        q.pop();
+                        for(int k=0;k<4;k++){
+                            int r=ii+dir[k];
+                            int c=jj+dir[3-k];
+                            if(r>=0 && c>=0 && r<grid.size() && c<grid[0].size() && grid[r][c]=='1' && !vis[r][c]){
+                                q.push(make_pair(r,c));
+                                vis[r][c]=1;
+                            }
+                        }
+                    }
                 }
-                if(ispos(t-1,d,grid)){
-                    q.push({t-1,d});
-                    grid[t-1][d]='0';
-                }
-                if(ispos(t,d+1,grid)){
-                    q.push({t,d+1});
-                    grid[t][d+1]='0';
-                }
-                if(ispos(t,d-1,grid)){
-                    q.push({t,d-1});
-                    grid[t][d-1]='0';
-                }
-                cout<<t<<" "<<d<<endl;
-                q.pop();
             }
         }
-    }
-    return ans;
+        return ans;
     }
 };
