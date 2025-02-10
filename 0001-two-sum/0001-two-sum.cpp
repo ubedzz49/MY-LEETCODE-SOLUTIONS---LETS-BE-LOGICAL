b71ca51e64f2f03d141e09d40cb9e6317715f9dd
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-02-10 14:30:08 +0530

    Time: 43 ms (32.14%), Space: 14 MB (92.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,16 +4,12 @@ public:
         vector<int> n(2);
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
-                if(i==j){
-
-                }
-                else{
+                
                     if(nums[i]+nums[j]==target){
                        n[0]=i;
                        n[1]=j;
                         return n;
                     }
-                }
             }
         }
         return {};
