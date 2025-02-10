d86ed96839d4ee2b0776a70a8303da5256073502
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-02-10 09:51:56 +0530

    Time: 6 ms (98.99%), Space: 54 MB (95.33%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<pair<int,double>> adj[n];
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
-        }
-        vector<double> prob(n,0.0);
-        prob[start]=1.0;
-        queue<pair<int,double>> q;
-        q.push({start,1.0});
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            for(auto v:adj[it.first]){
-                if(prob[v.first]<(it.second*v.second)){
-                    prob[v.first]=it.second*v.second;
-                    q.push({v.first,prob[v.first]});
-                }
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& prob, int s, int e) {
+        vector<double> sp(n,0);
+        sp[s]=1;
+        int b=true;
+        while(n>0 && b){
+            b=false;
+            n--;
+            for(int i=0;i<edges.size();i++){
+                if(sp[edges[i][1]]<sp[edges[i][0]]*prob[i]){ sp[edges[i][1]]=sp[edges[i][0]]*prob[i]; b=true;}
+                if(sp[edges[i][0]]<sp[edges[i][1]]*prob[i]){ sp[edges[i][0]]=sp[edges[i][1]]*prob[i]; b=true;}
             }
         }
-        return prob[end];
+        return sp[e];
     }
 };
