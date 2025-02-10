30c7ea2a7f8eb2fcff8eb33f3179a9b8d5b6788b
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-02-10 09:34:10 +0530

    Time: 57 ms (49.29%), Space: 72.4 MB (40.49%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -1,31 +1,25 @@
 class Solution {
-    public:
-        double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int src, int target) {
-                vector<pair<int,double>> adj[n];
-
-                        for(int i=0;i<edges.size();i++){
-                                    adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-                                                adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
-                                                        }
-
-                                                                vector<double> dist(n,0);
-                                                                        dist[src] = 1;
-                                                                                //  cout<<dist[src];
-                                                                                        priority_queue<pair<double,int>>pq;
-                                                                                                pq.push({1,src});
-
-                                                                                                        while(!pq.empty()){
-                                                                                                                    double dis = pq.top().first;
-                                                                                                                                int node = pq.top().second;
-                                                                                                                                            pq.pop();
-                                                                                                                                                        for(auto adjnode : adj[node]){
-                                                                                                                                                                        if(dis*adjnode.second > dist[adjnode.first]){
-                                                                                                                                                                                            dist[adjnode.first] = dis*adjnode.second;
-                                                                                                                                                                                                                pq.push({dist[adjnode.first],adjnode.first});
-                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                            // cout<<dist[src];
-                                                                                                                                                                                                                                                                    return dist[target];
-                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                        };
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        vector<pair<int,double>> adj[n];
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
+        }
+        vector<double> prob(n,0.0);
+        prob[start]=1.0;
+        queue<pair<int,double>> q;
+        q.push({start,1.0});
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            for(auto v:adj[it.first]){
+                if(prob[v.first]<(it.second*v.second)){
+                    prob[v.first]=it.second*v.second;
+                    q.push({v.first,prob[v.first]});
+                }
+            }
+        }
+        return prob[end];
+    }
+};
