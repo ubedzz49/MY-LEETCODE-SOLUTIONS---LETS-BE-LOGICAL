2ea50c862f4b09ef684d55af5727e03987206d45
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-02-28 09:40:58 +0530

    Time: 1185 ms (6%), Space: 773.8 MB (5.13%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,50 +1,43 @@
 class Solution {
 public:
-
-
-string shortestCommonSupersequence(string s1, string s2) {
-
-
-        int m=s1.size();
-        int n=s2.size();
-        vector<vector<int>> dp(m+1,vector<int> (n+1,0));
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+    string shortestCommonSupersequence(string s1, string s2) {
+        vector<pair<int,string>> dp(s2.size()+1,make_pair(0,""));
+        for(int i=0;i<s1.size();i++){
+            vector<pair<int,string>> dp1(s2.size()+1,make_pair(0,""));
+            for(int j=1;j<=s2.size();j++){
+                if(s1[i]==s2[j-1]) dp1[j]=make_pair(dp[j-1].first+1,dp[j-1].second+s1[i]);
+                else {
+                    if(dp1[j-1].first>dp[j].first) dp1[j]=dp1[j-1];
+                    else dp1[j]=dp[j];
+                }
             }
+            dp=dp1;
         }
+        string sup=dp[dp.size()-1].second;
+        int i=0;
+        int j=0;
         string ans="";
-
-        int i=m,j=n;
-        while(i>0 && j>0){
-            if(s1[i-1]==s2[j-1]){ ans+=s1[i-1]; i--; j--;}
-            else if(dp[i][j-1]>dp[i-1][j]) j--;
-            else i--;
-        }
-        reverse(ans.begin(),ans.end());
-        string fans="";
-        int i1=0;
-        int i2=0;
-        for(auto c:ans){
-            for(int i=i1;i<s1.size();i++){
-                if(s1[i]==c){
-                    i1=i+1;
-                    break;
-                }
-                fans+=s1[i];
+        for(auto it:sup){
+            while(it!=s1[i]){
+                ans.push_back(s1[i]);
+                i++;
             }
-            for(int i=i2;i<s2.size();i++){
-                if(s2[i]==c){
-                    i2=i+1;
-                    break;
-                }
-                fans+=s2[i];
+            while(it!=s2[j]){
+                ans.push_back(s2[j]);
+                j++;
             }
-            fans+=c;
+            ans+=it;
+            i++;
+            j++;
         }
-        for(int i=i1;i<s1.size();i++) fans+=s1[i];
-        for(int i=i2;i<s2.size();i++) fans+=s2[i];
-        return fans;
+        while(i<s1.size()){
+                ans.push_back(s1[i]);
+                i++;
+            }
+            while(j<s2.size()){
+                ans.push_back(s2[j]);
+                j++;
+            }
+        return ans;
     }
 };
