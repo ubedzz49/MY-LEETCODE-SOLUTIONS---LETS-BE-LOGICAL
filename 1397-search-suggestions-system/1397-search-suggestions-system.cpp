2e4773e61e241ef2f8d04e23166b54b3fe9ffed7
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-03-10 14:26:07 +0530

    Time: 9 ms (89.78%), Space: 36.9 MB (68.29%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.cpp
+++ 1397-search-suggestions-system/1397-search-suggestions-system.cpp
@@ -3,17 +3,17 @@ public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
         sort(products.begin(), products.end());
         vector<vector<string>> ans;
-        for(int i = 0; i < searchWord.size(); ++i){
-            string sub = searchWord.substr(0, i + 1);
-            auto it = lower_bound(products.begin(), products.end(), sub);
-            if(it == products.end()){
+        for(int i=0;i<searchWord.size();i++){
+            string sub = searchWord.substr(0,i+1);
+            auto it =lower_bound(products.begin(), products.end(), sub);
+            if(it==products.end()){
                 ans.push_back({});
             }else{
                 vector<string> search;
-                int j = it - products.begin();
-                int l = j + 3;
-                for(; j<l && j<products.size(); ++j){
-                    if((products[j].size() >= (i + 1)) && products[j].substr(0, i +1) == sub){
+                int j=it-products.begin();
+                int l=j+3;
+                for(j=j;j<l && j<products.size();j++){
+                    if((products[j].size()>=(i+1)) && products[j].substr(0,i+1)==sub){
                         search.push_back(products[j]);
                     }else{
                         break;
