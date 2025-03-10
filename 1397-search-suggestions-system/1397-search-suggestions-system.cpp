60adab9410479be0de89a25bbf352b383f7f9e4c
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-03-10 14:22:25 +0530

    Time: 8 ms (91.88%), Space: 36.8 MB (68.29%) - LeetHub

---
+++ 1397-search-suggestions-system/1397-search-suggestions-system.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(), products.end());
+        vector<vector<string>> ans;
+        for(int i = 0; i < searchWord.size(); ++i){
+            string sub = searchWord.substr(0, i + 1);
+            auto it = lower_bound(products.begin(), products.end(), sub);
+            if(it == products.end()){
+                ans.push_back({});
+            }else{
+                vector<string> search;
+                int j = it - products.begin();
+                int l = j + 3;
+                for(; j<l && j<products.size(); ++j){
+                    if((products[j].size() >= (i + 1)) && products[j].substr(0, i +1) == sub){
+                        search.push_back(products[j]);
+                    }else{
+                        break;
+                    }
+                }
+                ans.push_back(search);
+            }
+        }
+        return ans;
+    }
+};
