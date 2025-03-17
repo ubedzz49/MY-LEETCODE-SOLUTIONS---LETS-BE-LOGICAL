ba2e37e4471c5d2a8ae869e5f5ca81075b9215d6
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-03-17 13:43:27 +0530

    Time: 3 ms (60.38%), Space: 16.1 MB (71.41%) - LeetHub

---
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/1267-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode dummy(0);
+        dummy.next = head;
+        unordered_map<int, ListNode*> mpp;
+        int sm = 0;
+        
+        ListNode* h = &dummy;
+        
+        while (h) {
+            sm += h->val;
+            if (mpp.count(sm)) {
+                ListNode* prev = mpp[sm];
+                ListNode* temp = prev->next;
+                int sum = sm;
+                
+                while (temp != h) {
+                    sum += temp->val;
+                    mpp.erase(sum);
+                    temp = temp->next;
+                }
+                
+                prev->next = h->next; 
+            } else {
+                mpp[sm] = h;
+            }
+            h = h->next;
+        }
+        
+        return dummy.next;
+    }
+};
