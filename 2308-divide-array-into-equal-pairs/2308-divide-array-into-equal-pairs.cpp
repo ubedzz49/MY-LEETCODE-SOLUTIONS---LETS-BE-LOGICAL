694c3fadab2d82c0139af81670d3e2d33681a647
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-03-17 10:45:41 +0530

    Time: 0 ms (100%), Space: 16.7 MB (84.67%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
+        //USING BUT MANIPULATION
         int xr=0,xrr=0;
         for(auto it: nums) {xr=xr^it; xrr=xrr^(it+1);}
         if(xr==0 && xrr==0) return 1;
         return 0;
+
+        //USING HASHMAP
         unordered_map<int,bool> mpp;
         for(auto it:nums) mpp[it]=!mpp[it];
         for(auto it:mpp) if(it.second==1) return 0;
