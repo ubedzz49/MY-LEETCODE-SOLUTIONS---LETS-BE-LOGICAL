8ae93c41e5c5bf8be78e7859b281a43fda5a753b
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-03-17 16:15:27 +0530

    Time: 16 ms (6.98%), Space: 13.8 MB (8.35%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,19 +9,13 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        int c=0;
-        vector<ListNode*> v;
-        while(head!=nullptr){
-            if(head->val>100000){
-                int n=INT_MAX;
-                n-=(head->val);
-                return v[n];
-            }
-            v.push_back(head);
-            head->val=INT_MAX-c;
-            head=head->next;
-            c++;
+        unordered_map<ListNode*,int> mpp;
+        ListNode* slow=head;
+        while(slow){
+            if(mpp[slow]!=0) return slow;
+            mpp[slow]=1;
+            slow=slow->next;
         }
-        return head;
+        return nullptr;
     }
 };
