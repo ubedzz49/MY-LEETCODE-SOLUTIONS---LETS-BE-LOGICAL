207f36ff246424e806ea0517e22663d22ca217b7
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-03-23 19:06:49 +0530

    Time: 21 ms (12.55%), Space: 37.1 MB (62.05%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<int,int>> adj1={};
-        vector<vector<pair<int,int>>> adj(n,adj1);
-        for(auto i:roads){
-            adj[i[0]].push_back(make_pair(i[1],i[2]));
-            adj[i[1]].push_back(make_pair(i[0],i[2]));
+        vector<long long> dist(n, LONG_MAX);
+        vector<long long> ways(n, 0);
+        vector<vector<pair<int, int>>> adj(n);
+        for(auto road : roads) {
+            adj[road[0]].push_back({road[1], road[2]});
+            adj[road[1]].push_back({road[0], road[2]});
         }
-        priority_queue<vector<long long>,vector<vector<long long>>,greater<vector<long long>>> pq;
-        vector<long long> sp(n,LLONG_MAX);
-        sp[0]=0;
-        vector<int> spw(n,0);
-        spw[0]=1;
-        pq.push({0,0});
-        const int MOD = 1e9 + 7;  // Define the modulo constant
 
-        while(!pq.empty()){
-            long long dist=pq.top()[0];
-            int node=pq.top()[1];
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
+        pq.push({0, 0});
+        ways[0] = 1;
+        while(!pq.empty()) {
+            long long wt = pq.top().first;
+            long long node = pq.top().second;
             pq.pop();
-            for(auto i:adj[node]){
-                long long newDist = dist + i.second;
-                if(newDist == sp[i.first]){
-                    spw[i.first] = (spw[i.first] + spw[node]) % MOD;  // Apply modulo
+            for(auto it : adj[node]) {
+                long long u = it.first;
+                long long w = it.second;
+                if(wt + w < dist[u]) {
+                    dist[u] = wt + w;
+                    ways[u] = ways[node];
+                    pq.push({dist[u], u});
                 }
-                if(newDist < sp[i.first]){
-                    sp[i.first] = newDist;
-                    pq.push({sp[i.first], i.first});
-                    spw[i.first] = spw[node] % MOD;  // Apply modulo
+                else if (wt + w == dist[u]) {
+                    ways[u] = (ways[u] + ways[node]) % 1000000007;
                 }
             }
         }
-
-        return spw[n-1] % MOD;  // Apply modulo to the result
+        return ways[n - 1];
     }
 };
