01b144bb1b3a5c6a2d160552fe9be81de195a103
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-03-27 04:29:37 +0530

    Time: 0 ms (100%), Space: 46.4 MB (13.37%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,27 +1,6 @@
 class Solution {
 public:
     int maxScore(vector<int>& nums, int k) {
-
-        //TLE SOLUTIOMM 13/40
-        // queue<vector<int>> q;
-        // int ans=0;
-        // q.push({0,0,static_cast<int>(nums.size()-1),k});
-        // while(!q.empty()){
-        //     int cs=q.front()[0];
-        //     int s=q.front()[1];
-        //     int e=q.front()[2];
-        //     int kk=q.front()[3];
-        //     q.pop();
-        //     if(kk==0){
-        //         ans=max(ans,cs);
-        //         continue;
-        //     }
-        //     if(s<=e){
-        //         q.push({cs+nums[s],s+1,e,kk-1});
-        //         q.push({cs+nums[e],s,e-1,kk-1});
-        //     }
-        // }
-        // return ans;
         if(k==nums.size()) return accumulate(nums.begin(),nums.end(),0);
         int sum=0;
         for(int i=0;i<k;i++){
