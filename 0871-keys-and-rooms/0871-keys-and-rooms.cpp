af7da74ebba20bbdaba58c86e1639205bf936f78
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-05-12 16:02:36 +0530

    Time: 4 ms (12.83%), Space: 14.9 MB (27.69%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int ans=rooms.size();
+        unordered_map<int,bool> mpp;
+        mpp[0]=1;
+        ans--;
+        queue<int> q;
+        q.push(0);
+
+        while(!q.empty()){
+            int r=q.front();
+            q.pop();
+            for(auto it:rooms[r]){
+                if(!mpp[it]){
+                    ans--;
+                    q.push(it);
+                    mpp[it]=1;
+                }
+            }
+        }
+        if(ans==0) return true;
+        return false;
+    }
+};
