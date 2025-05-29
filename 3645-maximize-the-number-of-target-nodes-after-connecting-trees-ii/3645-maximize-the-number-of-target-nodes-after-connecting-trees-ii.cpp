96fdaf03655ae5d67f5db56e920373bd2f69f8ea
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-05-29 11:46:45 +0530

    Time: 271 ms (97.96%), Space: 338.3 MB (77.55%) - LeetHub

---
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dfs(int i, int p, const vector<vector<int>> &al, vector<bool> &parity, bool even = true) {
+    int targets = even;
+    parity[i] = even;
+    for (int j : al[i])
+        if (j != p)
+            targets += dfs(j, i, al, parity, !even);
+    return targets;
+}
+vector<vector<int>> adjacencyList(vector<vector<int>>& edges) {
+    vector<vector<int>> al(edges.size() + 1);
+    for (auto &e: edges) {
+        al[e[0]].push_back(e[1]);
+        al[e[1]].push_back(e[0]);            
+    }        
+    return al;
+}
+vector<int> maxTargetNodes(vector<vector<int>>& e1, vector<vector<int>>& e2) {
+    int m = e1.size() + 1, n = e2.size() + 1;
+    vector<bool> parity(m), ingnored(n);
+    int even1 = dfs(0, -1, adjacencyList(e1), parity), odd1 = m - even1;
+    int even2 = dfs(0, -1, adjacencyList(e2), ingnored), odd2 = n - even2;
+    vector<int> res(m);
+    for (int i = 0; i < m; ++i)
+        res[i] = max(even2, odd2) + (parity[i] ? even1 : odd1);
+    return res;
+}
+};
