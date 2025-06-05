5a4d7560f5db8e7b31963277fb3e1d3a05c6bcf3
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-06-05 20:04:49 +0530

    Time: 0 ms (100%), Space: 16.8 MB (60.18%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,101 +1,42 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
-
-bool isend(vector<vector<int>> grid){
-    for(int i=0;i<grid.size();i++){
-        for(int j=0;j<grid[0].size();j++){
-            int i1=i-1;
-            int i2=i+1;
-            int j1=j-1;
-            int j2=j+1;
-            if(i1>=0 && grid[i1][j]==1 && grid[i][j]==2){
-                return false;
-            }
-            if(j1>=0 && grid[i][j1]==1 && grid[i][j]==2){
-                return false;
-            }
-            if(i2<grid.size() && grid[i2][j]==1 && grid[i][j]==2){
-                return false;
-            }
-            if(j2<grid[0].size() && grid[i][j2]==1 && grid[i][j]==2){
-                return false;
-            }
-        }
-    }
-    return true;
-}
     int orangesRotting(vector<vector<int>>& grid) {
-int co=0;int c1=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==2){
-                    co=1;
-                }
-                if(grid[i][j]==1){
-                    c1=1;
-                }
+        int n = grid.size();
+        int m = grid[0].size();
+        int tot = 0, cnt = 0;
+        queue<pair<int, int>> q;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2)
+                    q.push({i, j});
+                if (grid[i][j] != 0)
+                    tot++;
             }
         }
-        if(c1==0){
-    return 0;
-}
-if(co==0){
-    return -1;
-}
-
-if(isend(grid)==1){
-    for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
-                    return -1;
+        int dx[4] = {0, 0, 1, -1};
+        int dy[4] = {1, -1, 0, 0};
+        int ans = 0;
+        while (!q.empty()) {
+            int k = q.size();
+            cnt += k;
+            while (k--) {
+                int y = q.front().second, x = q.front().first;
+                q.pop();
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i], ny = y + dy[i];
+                    if (nx < 0 || ny < 0 || nx >= n || ny >= m ||
+                        grid[nx][ny] != 1)
+                        continue;
+                    grid[nx][ny] = 2;
+                    q.push({nx, ny});
                 }
             }
+            if (!q.empty())
+                ans++;
         }
-        return 0;
-}
-
-        int c=0;
-        while(isend(grid)==0){
-            c=c+1;
-            cout<<c<<" ";
-            vector<vector<int>> grid1=grid;
-            for(int i=0;i<grid.size();i++){
-                for(int j=0;j<grid[0].size();j++){
-            int i1=i-1;
-            int i2=i+1;
-            int j1=j-1;
-            int j2=j+1;
-            if(i1>=0 && grid[i1][j]==1 && grid[i][j]==2){
-                if(grid[i1][j]!=0){
-                    grid1[i1][j]=2;
-                }
-            }
-            if(j1>=0 && grid[i][j1]==1 && grid[i][j]==2){
-                if(grid[i][j1]!=0){
-                    grid1[i][j1]=2;
-                }
-            }
-            if(i2<grid.size() && grid[i2][j]==1 && grid[i][j]==2){
-                if(grid[i2][j]!=0){
-                    grid1[i2][j]=2;
-                }
-            }
-            if(j2<grid[0].size() && grid[i][j2]==1 && grid[i][j]==2){
-                if(grid[i][j2]!=0){
-                    grid1[i][j2]=2;
-                }
-            }
-                }
-            }
-            grid=grid1;
-        }
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-            }
-        }
-        return c;
+        if (tot == cnt)
+            return ans;
+        return -1;
     }
 };
