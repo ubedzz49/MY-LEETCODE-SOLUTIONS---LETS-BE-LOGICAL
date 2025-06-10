ffb34eeb957d9c510c6d4018ea8103fc40395a00
ubedzz49 <121815288+ubedzz49@users.noreply.github.com>
2025-06-10 11:11:00 +0530

    Time: 3 ms (34.81%), Space: 9.8 MB (40.32%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -4,17 +4,13 @@ public:
         unordered_map<char,int> mpp;
         for(auto it:s) mpp[it]++;
         int odmx=INT_MIN;
-        int odmn=INT_MAX;
-        int evmx=INT_MIN;
         int evmn=INT_MAX;
         for(auto it:mpp){
             if(it.second & 1){
-                odmn=min(odmn,it.second);
                 odmx=max(odmx,it.second);
             }
             else{
                 evmn=min(evmn,it.second);
-                evmx=max(evmx,it.second);
             }
         }
         return (odmx-evmn);
